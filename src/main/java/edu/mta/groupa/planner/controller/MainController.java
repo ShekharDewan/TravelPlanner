38b7b5f77fe9111de258b8fb2efa24d2fe9351ff
Maryse Arseneau <mgarseneau@mta.ca>
2019-02-18 11:59:27 -0400

    Added create itinerary, accommodation, and reservation code

--- src/main/java/edu/mta/groupa/planner/controller/MainController.java
+++ src/main/java/edu/mta/groupa/planner/controller/MainController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.mta.groupa.planner.model.Accommodation;
 import edu.mta.groupa.planner.model.Address;
+import edu.mta.groupa.planner.model.Itinerary;
+import edu.mta.groupa.planner.model.Reservation;
 import edu.mta.groupa.planner.model.Trip;
 import edu.mta.groupa.planner.repository.TripRepository;
 
@@ -87,11 +89,60 @@ public class MainController {
     @PostMapping("/trip/{id}/accommodation/add")
     public String addAccomodation(Model model, @PathVariable("id") long id, @Valid Accommodation accomodation, BindingResult result) {
     	if (result.hasErrors()) {
-            return "edit-trip";
+            return "add-accommodation";
         }
     	
     	Trip trip = tripRepository.findById(id).get();
     	trip.getAccomodations().add(accomodation);
+    	tripRepository.save(trip);
+    	model.addAttribute("trip", trip );
+                
+        return "trip"; //view
+    }
+    
+    @GetMapping("/trip/{id}/reservation/add")
+    public String showReservationForm(@PathVariable("id") long id, Model model) {
+ 
+    	Reservation reservation = new Reservation();
+    	reservation.setAddress(new Address());
+    	Trip trip = tripRepository.findById(id).get();
+        model.addAttribute("reservation", reservation);
+        model.addAttribute("trip", trip);
+        return "add-reservation";
+    }
+    
+    @PostMapping("/trip/{id}/reservation/add")
+    public String addReservation(Model model, @PathVariable("id") long id, @Valid Reservation reservation, BindingResult result) {
+    	if (result.hasErrors()) {
+            return "add-reservation";
+        }
+    	
+    	Trip trip = tripRepository.findById(id).get();
+    	trip.getReservations().add(reservation);
+    	tripRepository.save(trip);
+    	model.addAttribute("trip", trip );
+                
+        return "trip"; //view
+    }
+    
+    @GetMapping("/trip/{id}/itinerary/add")
+    public String showItineraryForm(@PathVariable("id") long id, Model model) {
+ 
+    	Itinerary itinerary = new Itinerary();
+    	Trip trip = tripRepository.findById(id).get();
+        model.addAttribute("itinerary", itinerary);
+        model.addAttribute("trip", trip);
+        return "add-itinerary";
+    }
+    
+    @PostMapping("/trip/{id}/itinerary/add")
+    public String addItinerary(Model model, @PathVariable("id") long id, @Valid Itinerary itinerary, BindingResult result) {
+    	if (result.hasErrors()) {
+            return "add-itinerary";
+        }
+    	
+    	Trip trip = tripRepository.findById(id).get();
+    	trip.getItineraries().add(itinerary);
     	tripRepository.save(trip);
     	model.addAttribute("trip", trip );
                 
