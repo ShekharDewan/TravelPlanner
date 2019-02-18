325eba6ac7fb89e517dde14b7a5c68b018b2a7f8
Jennifer Firsoff <Jennifer Firsoff@oogamama>
2019-02-17 20:07:15 -0400

    Adding editing functionality for basic trip information

--- src/main/java/edu/mta/groupa/planner/controller/MainController.java
+++ src/main/java/edu/mta/groupa/planner/controller/MainController.java
@@ -1,12 +1,24 @@
 package edu.mta.groupa.planner.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import edu.mta.groupa.planner.model.Trip;
 import edu.mta.groupa.planner.repository.TripRepository;
 
 @Controller
@@ -30,6 +42,41 @@ public class MainController {
     public String tripPage(Model model, @PathVariable("id") long id) {
         model.addAttribute("trip", tripRepository.findById(id).get() );
         return "trip"; //view
+    }
+    
+    @GetMapping("/trip/{id}/edit")
+    public String showEditForm(@PathVariable("id") long id, Model model) {
+ 
+        model.addAttribute("trip", tripRepository.findById(id).get());
+        return "edit-trip";
+    }
+    
+    @PostMapping("/trip/{id}/update")
+    public String updateTrip(@PathVariable("id") long id, @Valid Trip trip, 
+      BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            trip.setId(id);
+            return "edit-trip";
+        }
+        Trip oldTrip = tripRepository.findById(id).get();
+
+        oldTrip.setNotes(trip.getNotes());
+        oldTrip.setStart(trip.getStart());
+        oldTrip.setEnd(trip.getEnd());
+        oldTrip.setTitle(trip.getTitle());
+        oldTrip.setDescription(trip.getDescription());
+        
+        tripRepository.save(oldTrip);
+        model.addAttribute("trip", tripRepository.findById(id).get()); 
+        return "trip";
+    }
+    
+    @InitBinder
+    public void dataBinding(WebDataBinder binder) {
+    	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    	dateFormat.setLenient(false);
+    	binder.registerCustomEditor(Date.class, "start", new CustomDateEditor(dateFormat, true));
+    	binder.registerCustomEditor(Date.class, "end", new CustomDateEditor(dateFormat, true));
     }
 
 }
